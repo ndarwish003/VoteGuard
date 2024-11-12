1b57bc6ebd044bcc1483ec9ff3df76c44a630bb0
ndarwish003 <s2211132104@ku.edu.kw>
2024-11-12 17:23:54 +0300

    Update

--- FACE_RECOGNITION/FR.py
+++ FACE_RECOGNITION/FR.py
@@ -13,6 +13,7 @@ known_face_names = []
 
 
 folder_dir = "images"
+
 for image in os.listdir(folder_dir):
     if (image.endswith(".jpg")):
         face_image = face_recognition.load_image_file("images/"+image)
@@ -29,7 +30,6 @@ process_this_frame = True
 while True:
     
     ret, frame = video_capture.read()
-
     
     if process_this_frame:
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -42,6 +42,7 @@ while True:
             name = "Unknown"
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
+            
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
                 
@@ -52,23 +53,18 @@ while True:
 
    
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-       
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name + " ", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)        
     
     cv2.imshow('Video', frame)
 
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
